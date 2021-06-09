34087e66bc273f77fcbcab30a73fd1fc52c65a26
bingle.yu@gmail.com <bingle.yu@gmail.com>
2021-06-09 21:31:04 +0800

    Add more ...

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -20,8 +20,7 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user.
-    if topic.owner != request.user:
-        raise Http404   
+    check_topic_owner(topic, request)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -48,6 +47,8 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic, request)
+
     if request.method != 'POST':
         # No data submitted; create a blank form.
         form = EntryForm()
@@ -68,9 +69,8 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    if topic.owner != request.user:
-        raise Http404
-    
+    check_topic_owner(topic, request)
+
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
         form = EntryForm(instance=entry)
